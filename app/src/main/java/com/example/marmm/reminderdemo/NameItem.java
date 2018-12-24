b559c356bbb76fa11ddde502ea5e943512989852
Jmaster2710 <joel_kroon@hotmail.com>
2018-12-24 14:42:52 +0100

    Added Name Item, and added Retrofit

---
+++ app/src/main/java/com/example/marmm/reminderdemo/NameItem.java
@@ -0,0 +1,53 @@
+package com.example.marmm.reminderdemo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class NameItem {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("surname")
+    @Expose
+    private String surname;
+    @SerializedName("gender")
+    @Expose
+    private String gender;
+    @SerializedName("region")
+    @Expose
+    private String region;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+}
