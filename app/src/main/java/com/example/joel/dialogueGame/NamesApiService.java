536a8560709a225b8b54b69a4dc703ac7f1b3860
Jmaster2710 <joel_kroon@hotmail.com>
2019-01-04 15:04:39 +0100

    Changed the way you can set the region

--- app/src/main/java/com/example/joel/dialogueGame/NamesApiService.java
+++ app/src/main/java/com/example/joel/dialogueGame/NamesApiService.java
@@ -4,17 +4,19 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface NamesApiService {
 
     String BASE_URL = "https://uinames.com/";
+    String REGION = "netherlands";
 
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
-    @GET("/api/")
+    @GET("/api/?region=" + REGION)
 
     Call<NameItem> getNames();
 }
